c013421fd3e5fae8a72a5591bdf60737377ffd63
Ryan Smith <ryan.smith@pobox.com>
2016-03-16 10:04:45 +0000

    bug: save does not work in FF

--- client/_common/S3Access.ts
+++ client/_common/S3Access.ts
@@ -4,12 +4,19 @@ namespace S3Access {
     /**
      * Upload file to application S3 bucket
      */
-    export function putFile(fileName: string, fileType: string, data: Blob|string) {
+    export function putFile(fileName: string, fileType: string, data: Blob | string) {
+
+        // https://github.com/aws/aws-sdk-js/issues/190   
+        if (navigator.userAgent.match(/Firefox/) && !fileType.match(/;/)) {
+            var charset = '; charset=UTF-8';
+            fileType += charset;
+        }
+
         const signUrl = `/api/storage/access?fileName=${fileName}&fileType=${fileType}`;
         // get signed URL
         $.getJSON(signUrl)
             .done(signResponse => {
-                
+
                 // PUT file
                 const putRequest = {
                     method: "PUT",
@@ -21,29 +28,37 @@ namespace S3Access {
                     processData: false,
                     contentType: fileType
                 };
+
                 $.ajax(putRequest)
-                .done(putResponse => {
-                    console.log("uploaded file", signResponse.url)
-                })
-                .fail(err => {
-                    console.error("error uploading to S3", err);
-                });
-                
+                    .done(putResponse => {
+                        console.log("uploaded file", signResponse.url)
+                    })
+                    .fail(err => {
+                        console.error("error uploading to S3", err);
+                    });
+
             })
             .fail(err => {
                 console.error("error on /api/storage/access", err);
             });
     }
-    
+
     /**
      * Download file from bucket
      */
     export function getFile(fileName: string): JQueryPromise<any> {
-        return $.getJSON(`/api/storage/url?fileName=${fileName}`)
+        return $.ajax({
+            url: `/api/storage/url?fileName=${fileName}`,
+            dataType: "json"
+        })
             .then(response => {
                 console.log("downloading", response.url);
-                return $.getJSON(response.url);
+                return $.ajax({
+                    url: response.url,
+                    dataType: "json",
+                    cache: false
+                });
             });
     }
-    
+
 }
